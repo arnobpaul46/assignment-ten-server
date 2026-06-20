67bd419c95181ac3bae0b546c3cc3df11dfd6ab4
arnobpaul46 <arnobpaulbd@gmail.com>
2026-06-20 12:08:28 +0600

    admin reader writer api created

--- index.js
+++ index.js
@@ -15,25 +15,41 @@ async function run() {
   try {
     await client.connect();
     const db = client.db("fableDB");
+
+
+    // Collection 
     const bookCollection = db.collection("featured_books");
+    const userCollection = db.collection("user");
+
+
+
 
     console.log("MongoDB Atlas Connected!");
-
-    
+    // home slider api
     app.get('/api/featured-books', async (req, res) => {
       const result = await bookCollection.find({ isFeatured: true }).toArray();
       res.send(result);
     });
+    // admin api
+    app.get('/api/admin/users', async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
 
-    
-    app.post('/api/add-book', async (req, res) => {
-      const book = req.body;
-      const result = await bookCollection.insertOne(book);
+    // writer and reader api
+    app.patch('/api/admin/update-role/:id', async (req, res) => {
+      const id = req.params.id;
+      const { role } = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = { $set: { role: role } };
+      const result = await userCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
 
-  } finally {}
+  } catch (error) {
+    console.error("Connection Error:", error);
+  }
 }
 run().catch(console.dir);
 
-app.listen(process.env.PORT, () => console.log("🚀 Server running on port 5000"));
+app.listen(process.env.PORT, () => console.log(" Server running on port 5000"));
